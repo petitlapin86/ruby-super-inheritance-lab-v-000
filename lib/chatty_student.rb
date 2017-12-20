e80901c9c8317c24b49fa09134c0cf54d77c6825
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 14:54:19 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,13 +2,13 @@ class ChattyStudent < Student # should inherit attributes from student class
 
 
   def hello
-    super
+    super # using super to add on to the hello method inherited from student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
   def raise_hand
-    super
-    super
+    super # a very chatty student says "pick me" 10 times
+    super # using super to call in the puts "pick me"
     super
     super
     super
